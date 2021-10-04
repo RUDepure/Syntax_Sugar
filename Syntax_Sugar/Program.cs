8700cc3c1cc3703bee1a046b70df06aa11e5cb9d
RUDepure <alexrum92@gmail.com>
2021-10-04 10:12:01 -0500

    initial commit

---
+++ Syntax_Sugar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syntax_Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine.";
+            }
+            else
+            {
+                response = answer + " is greater or equal than nine.";
+            }*/
+
+            int answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine." : $"{answer} greater than or equal to nine.";
+            
+            Console.WriteLine(response);
+
+        }
+    }
+}
